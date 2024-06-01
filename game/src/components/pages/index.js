6090b8e5863283c0a5f6b838530e8a5491ddf221
xinweixw <chanxinwei3@gmail.com>
2024-06-01 11:32:46 +0800

    Remove Chapter 3 export

--- game/src/components/pages/index.js
+++ game/src/components/pages/index.js
@@ -4,5 +4,4 @@ export * from "./Chapter1";
 export * from "./Chapter1EducationalPage"; 
 export * from "./Chapter2";
 export * from "./Chapter2Educationalpage";
-export * from "./Chapter3";
 export * from "./Epilogue";
